4cbf887f9bff97d1d4680b4bf141317ca10b5837
Terminal1337 <95563109+Terminal1337@users.noreply.github.com>
2022-03-21 00:19:38 -0700

    Update hwid_login.py

--- hwid_login.py
+++ hwid_login.py
@@ -6,7 +6,7 @@ hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1
 r = requests.get(pastebin)
 
 if hwid in r.text:
-    pass
     print("Successfully Logged In!!")
+    pass
 else:
     sys.exit("Login Failure")
