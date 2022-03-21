edfb84ced8d68b50cf6bc8166408a5955203f00d
Terminal1337 <95563109+Terminal1337@users.noreply.github.com>
2022-03-21 00:16:15 -0700

    Create hwid_login.py

---
+++ hwid_login.py
@@ -0,0 +1,12 @@
+import requests
+import subprocess
+import sys
+pastebin = 'your paste bin raw link that has the hwid of the user'
+hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
+r = requests.get(pastebin)
+
+if hwid in r.text:
+    pass
+    print("Successfully Logged In!!")
+else:
+    sys.exit("Login Failure")
